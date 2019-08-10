37027c181b39c4957f0a3c26ab88cc12180a3f7b
chamaka wijesinghe <35440106+chamaka1@users.noreply.github.com>
2019-08-10 23:21:56 +1000

    Add files via upload

--- TestScript.cs
+++ TestScript.cs
@@ -28,7 +28,7 @@ public class TestScript : MonoBehaviour
     public const double b = 0.8; // radius of central region
     public double t_final; // duration of sim
     public double dt; // step size
-
+    public int PositionScaler;
 
     public double[] xp = new double[particle_no];
     public double[] yp = new double[particle_no];
@@ -47,13 +47,21 @@ public class TestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        PositionScaler = 1000;
         double[] initial_position = {2,-1,0} ;
         double[] initial_speed = {-0.1,-0.15,0} ;
         // sim conditions
-        t_final = 1.2f * Math.Pow(10, -5);
-        dt = 1f * Math.Pow(10, -10); 
+        t_final = 1.2 * Math.Pow(10, -5);
+        dt = 1 * Math.Pow(10, -10); 
         //particle_no = 1 ;
         ds = 3/particle_no; // spacing diameter of whole coil
+
+        // environment variables
+        mu = 4 * Math.PI * Math.Pow(10, -7) ;
+        q = 1.60217 * Math.Pow(10,-19);
+        m = 1.67262 * Math.Pow(10, -27);
+        c = 3 * Math.Pow(10, 8);
+        g = 9.81;
         
                 
 
@@ -62,98 +70,130 @@ public class TestScript : MonoBehaviour
             xp[i] = initial_position[0];
             yp[i] = initial_position[1];
             zp[i] = ds * particle_no - ds*(i+1);
-            vxp[i] = initial_speed[0];
-            vyp[i] = initial_speed[1];
-            vzp[i] = initial_speed[2];
-
+            vxp[i] = initial_speed[0] * c;
+            vyp[i] = initial_speed[1] * c;
+            vzp[i] = initial_speed[2] * c;
+            
+            
+            
         }
         
-        // environment variables
-        mu = 4 * Math.PI * Math.Pow(10, -7) ;
-        q = 1.60217 * Math.Pow(10,-19);
-        m = 1.67262 * Math.Pow(10, -27);
-        c = 3 * Math.Pow(10, 8);
-        g = 9.81;
+    
 
 
         rb = GetComponent<Rigidbody>();
         
         sphere_position = new Vector3((float) xp[0], (float)yp[0],(float) zp[0]);
-        GetComponent<Rigidbody>().position = sphere_position;
+        
+        GetComponent<Transform>().localPosition = sphere_position*PositionScaler;
+        //GetComponent<Rigidbody>().localPosition = sphere_position;
         //Rigidbody.MovePosition(position);
         
        
-        Debug.Log(sphere_position);
+        //Debug.Log(sphere_position);
     }
 
-    // fixed update must be set to be same as dt
+    
+
+    // fixed update must be set to be same as dtt
     void FixedUpdate()
     {
-        for (int i = 0; i < particle_no ; i++) // loop for each particle
+        
+        
+        for (int j = 0; j < 60; j++)
         {
+            for (int i = 0; i < particle_no ; i++) // loop for each particle
+            {
 
-            double x = xp[i];
-            double y = yp[i];
-            double z = zp[i];
-            double vx = vxp[i];
-            double vy = vyp[i];
-            double vz = vzp[i];
-            double gamma = gammap[i];
+                double x = xp[i];
+                double y = yp[i];
+                double z = zp[i];
+                double vx = vxp[i] ;
+                double vy = vyp[i] ;
+                double vz = vzp[i] ;
+                
+                //Debug.Log(vxp[0]);
+                 
+                double gamma = 1/Math.Sqrt(1-(Math.Pow(vx, 2) + Math.Pow(vy, 2) + Math.Pow(vz, 2))/Math.Pow(c, 2));
+                
+                //double phi = Math.Atan2(1, 2); // double matlab phi ??
+                
+                
+                //distance = sqrt( z^2 + (x-(a+b)*cos(phi))^2 + (y-(a+b)*sin(phi))^2 );
+                double[] input = new double[]{x, y, z};
+                double[] B = toroid(input);
+                //Debug.Log(phi);
+                double Bx = B[0];
+                double By = B[1];
+                double Bz = B[2];
+                //Debug.Log(Bx);
+                double ax = q/(gamma*m) * (vy*Bz-vz*By);
+                double ay = q/(gamma*m) * (vz*Bx-vx*Bz); 
+                double az = q/(gamma*m) * (vx*By-vz*Bx) -g;
+                //Debug.Log(gamma);
+                //Debug.Log(ax);
+                vx = vx + ax*dt;
+                vy = vy + ay*dt;
+                vz = vz + az*dt;
+                //Debug.Log(vx);
+                //Debug.Log(Bz);
+                
+               
+                // error occurs with this
+                //sim error
+                double initial_e = Math.Sqrt(Math.Pow(0.1, 2) + Math.Pow(0.15, 2) + Math.Pow(0, 2));
+                double current_e = (Math.Sqrt((Math.Pow(vx, 2) + Math.Pow(vy, 2) + Math.Pow(vz, 2)) / Math.Pow(c, 2)));
 
-            double theta = Math.Atan2(y, x);
-            //distance = sqrt( z^2 + (x-(a+b)*cos(phi))^2 + (y-(a+b)*sin(phi))^2 );
-            double[] input = new double[]{x, y, z};
-            double[] B = toroid(input);
+                double factor = initial_e / current_e; 
+                vxp[i] = vx * factor;
+                vyp[i] = vy * factor;
+                vzp[i] = vz * factor;
+                //Debug.Log(factor);
 
-            double Bx = B[0];
-            double By = B[1];
-            double Bz = B[2];
-
-            double ax = q/(gamma*m) * (vy*Bz-vz*By);
-            double ay = q/(gamma*m) * (vz*Bx-vx*Bz); 
-            double az = q/(gamma*m) * (vx*By-vz*Bx) -g;
-            Debug.Log(ax);
-            vx = vx + ax*dt;
-            vy = vy + ay*dt;
-            vz = vz + az*dt;
-            //Debug.Log(x);
-            //Debug.Log(vx);  
-
-            //sim error
-            vxp[i] = vx * Math.Sqrt(Math.Pow(initial_speed[0], 2) + Math.Pow(initial_speed[1], 2) + Math.Pow(initial_speed[2], 2)) / (Math.Sqrt(Math.Pow(vx, 2) + Math.Pow(vy, 2) + Math.Pow(vz, 2))) / Math.Pow(c, 2);
-            vyp[i] = vy * Math.Sqrt(Math.Pow(initial_speed[0], 2) + Math.Pow(initial_speed[1], 2) + Math.Pow(initial_speed[2], 2)) / (Math.Sqrt(Math.Pow(vx, 2) + Math.Pow(vy, 2) + Math.Pow(vz, 2))) / Math.Pow(c, 2);
-            vzp[i] = vz * Math.Sqrt(Math.Pow(initial_speed[0], 2) + Math.Pow(initial_speed[1], 2) + Math.Pow(initial_speed[2], 2)) / (Math.Sqrt(Math.Pow(vx, 2) + Math.Pow(vy, 2) + Math.Pow(vz, 2))) / Math.Pow(c, 2);
-
-
-            xp[i] = x + vxp[i]*dt;
-            yp[i] = y + vyp[i]*dt;
-            zp[i] = z + vzp[i]*dt;
-         
+                xp[i] = x + vxp[i]*dt;
+                yp[i] = y + vyp[i]*dt;
+                zp[i] = z + vzp[i]*dt;
+                //Debug.Log(xp[i]);
+                //Debug.Log(zp[i]);
+                //Debug.Log(vxp[i]);
+            
+            }
+        
+        
         }
-
-        sphere_position = new Vector3((float) xp[0], (float) yp[0], (float) zp[0]);
-        GetComponent<Rigidbody>().position = sphere_position;
+        //changed vectors to coincide with model on AR
+        sphere_position = new Vector3((float) xp[0], (float) zp[0], (float) yp[0]);
+        GetComponent<Transform>().localPosition = sphere_position*PositionScaler;
+        //Instantiate(GetComponent<Transform>());
+        
+        //GetComponent<Rigidbody>().localPosition = sphere_position;
         //Debug.Log(sphere_position);
-
-
-
+        //Debug.Log(xp[0]);
+        //Debug.Log(yp[0]);
+        //Debug.Log(zp[0]);
     }
+
+    
 
     public double[] toroid(double[] input) {
         double[] B = new double[]{0, 0, 0};
 
         double r = Math.Sqrt(Math.Pow((double)input[0], 2) + Math.Pow((double)input[1], 2));
-        double B_r = -2f * (1 / r) * input[2] * Math.Pow(r,2);
-        double B_z = -1f / r *((4f * r - 4f * Math.Pow(r , 3)) - (2 * r * Math.Pow(input[2] , 2)));
-        double B_theta = 0f;
+        double B_r = 1d / r *-2d * input[2] * Math.Pow((double)r,2);
+        double B_z = 1d / r * -(4d * r - 4d * Math.Pow((double)r , 3) - 2d * r * Math.Pow((double)input[2] , 2));
+        double B_theta = 0d;
 
-        double theta = Math.Atan2(input[1], input[0]);
+        double theta = Math.Atan2(input[1], input[0]); // maybe wrong
 
         B[0] = Math.Cos(theta) * B_r - Math.Sin(theta) * B_theta;
-        B[1] = Math.Sin(theta) * B_r + Math.Cos(theta) - B_theta;
+        B[1] = Math.Sin(theta) * B_r + Math.Cos(theta) * B_theta;
         B[2] = B_z;
-
+        
+        //Debug.Log(B_r);
+        //Debug.Log(input[2]);
         return B;
     }   
 }
+
+
 
